1156514b79916765d70d76bd256122fecbf7800e
Danilo Spinella <danyspin.ds@gmail.com>
2016-10-24 23:51:45 +0200

    Fixes ci

--- tests/bottest.php
+++ tests/bottest.php
@@ -8,6 +8,6 @@ class BotTest extends PHPUnit_Framework_TestCase
     public function setUpBot() {
         $this->bot = new DanySpin97\MyAddressBookBot\Bot("token");
         $this->bot->connectToRedis();
-        $this->bot->connectToDatabase("mysql", "hello_world_test", "root", "mysql");
+        $this->bot->connectToDatabase("mysql", "hello_world_test", "travis", "");
     }
 }
