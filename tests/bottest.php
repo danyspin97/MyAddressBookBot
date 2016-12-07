da987bc845d3ef1c1e9c2d0796e2c30525602411
Danilo Spinella <danyspin.ds@gmail.com>
2016-12-07 22:22:43 +0100

    Fix bot test class in tests

--- tests/bottest.php
+++ tests/bottest.php
@@ -7,7 +7,7 @@ class BotTest extends PHPUnit_Framework_TestCase
     public $bot;
 
     public function testBot() {
-        $this->bot = new DanySpin97\MyAddressBookBot\Bot("token");
+        $this->bot = new DanySpin97\MyAddressBookBot("token");
         $this->bot->connectToRedis();
         $this->bot->connectToDatabase("pgsql", "travis_ci_test", "postgresql", "");
     }
