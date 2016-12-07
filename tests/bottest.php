cceea05d27e1e3a9a7eb23866435bed88a10bb9a
Danilo Spinella <danyspin.ds@gmail.com>
2016-12-07 22:01:09 +0100

    Fix test for travis CI

--- tests/bottest.php
+++ tests/bottest.php
@@ -5,7 +5,8 @@ require './vendor/autoload.php';
 class BotTest extends PHPUnit_Framework_TestCase
 {
     public $bot;
-    public function setUpBot() {
+
+    public function testBot() {
         $this->bot = new DanySpin97\MyAddressBookBot\Bot("token");
         $this->bot->connectToRedis();
         $this->bot->connectToDatabase("pgsql", "travis_ci_test", "postgresql", "");
